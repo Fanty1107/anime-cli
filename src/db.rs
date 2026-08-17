fc8ceb4af8eb15a95c42bf9d73f5cc1ff8bec743
Fanty1107 <edufantinlemes@gmail.com>
2026-08-17 18:13:35 -0300

    add:remove,update;fix:main code, args

--- src/db.rs
+++ src/db.rs
@@ -1,11 +1,14 @@
 use core::fmt;
 use rusqlite::{Connection, Result as ResultSql};
 use comfy_table::Table;
-
+use std::env;
+use std::fs;
+use std::path::PathBuf;
 pub struct Anime {
     pub id: Option<u32>,
     pub nome: String,
     pub num_ep: u32,
+    pub cur_ep: u32,
 }
 
 impl Anime {
@@ -14,6 +17,7 @@ impl Anime {
             id: None,
             nome: n.to_string(),
             num_ep: n_ep,
+            cur_ep: 0,
         }
     }
 }
@@ -25,7 +29,12 @@ impl fmt::Display for Anime {
 }
 
 pub fn new_db() -> ResultSql<Connection> {
-    let conn = Connection::open("database.db")?;
+    let home_dir = env::var("HOME").expect("Error: directory home not found");
+    let mut db_dir = PathBuf::from(home_dir);
+    db_dir.push(".anime_cli");
+    fs::create_dir_all(&db_dir).expect("Error: could not create directory");
+    db_dir.push("database.db");
+    let conn = Connection::open(db_dir)?;
     Ok(conn)
 }
 
@@ -34,6 +43,7 @@ pub fn init_db(conn: &Connection) -> ResultSql<()> {
         "CREATE TABLE IF NOT EXISTS anime(
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL UNIQUE,
+            current_ep INTEGER NOT NULL,
             episodios INTEGER NOT NULL
         )",
         [],
@@ -43,8 +53,8 @@ pub fn init_db(conn: &Connection) -> ResultSql<()> {
 
 pub fn add_data(conn: &Connection, anime: &mut Anime) -> ResultSql<bool> {
     let lines = conn.execute(
-        "INSERT OR IGNORE INTO anime (name, episodios) VALUES (?1,?2)",
-        rusqlite::params![anime.nome, anime.num_ep],
+        "INSERT OR IGNORE INTO anime (name, episodios, current_ep) VALUES (?1,?2,?3)",
+        rusqlite::params![anime.nome, anime.num_ep, anime.cur_ep],
     )?;
     
     if lines > 0 {
@@ -57,24 +67,74 @@ pub fn add_data(conn: &Connection, anime: &mut Anime) -> ResultSql<bool> {
 }
 
 pub fn show_db(conn: &Connection) -> ResultSql<()> {
-    let mut stmt = conn.prepare("SELECT id, name, episodios FROM anime")?;
+    let mut stmt = conn.prepare("SELECT id, name, current_ep, episodios FROM anime")?;
 
     let anime_iter = stmt.query_map([], |row| {
         Ok((
-            row.get::<_, i32>(0)?,
+            row.get::<_, u32>(0)?,
             row.get::<_, String>(1)?,
             row.get::<_, u32>(2)?,
+            row.get::<_, u32>(3)?,
         ))
     })?;
     
     let mut table = Table::new();
-    table.set_header(vec!["ID", "Name", "Episodes"]);
+    table.set_header(vec!["ID", "Name","Current_Ep","Episodes"]);
     
     for anime in anime_iter {
-        let (id, nome, eps) = anime?;
-        table.add_row(vec![id.to_string(), nome, eps.to_string()]);
+        let (id, nome,cur_eps, eps) = anime?;
+        table.add_row(vec![id.to_string(), nome,cur_eps.to_string(), eps.to_string()]);
     }
     
     println!("{table}");
     Ok(())
+}
+pub fn search_ani(conn: &Connection, termo: &str) -> ResultSql<Anime> {
+    if let Ok(id_num) = termo.parse::<u32>() {
+        conn.query_row(
+            "SELECT id, name, current_ep, episodios FROM anime WHERE id = ?1",
+            [id_num],
+            |row| {
+                Ok(Anime {
+                    id: Some(row.get(0)?),
+                    nome: row.get(1)?,
+                    cur_ep: row.get(2)?,
+                    num_ep: row.get(3)?,
+                })
+            },
+        )
+    } else {
+        conn.query_row(
+            "SELECT id, name, current_ep, episodios FROM anime WHERE name = ?1",
+            [termo],
+            |row| {
+                Ok(Anime {
+                    id: Some(row.get(0)?),
+                    nome: row.get(1)?,
+                    cur_ep: row.get(2)?,
+                    num_ep: row.get(3)?,
+                })
+            },
+        )
+    }
+}
+
+pub fn update_ep(conn: &Connection, ani: &mut Anime, seen_eps: u32) -> ResultSql<()>{
+    ani.cur_ep += seen_eps;
+    conn.execute("UPDATE anime SET current_ep = ?1 WHERE id = ?2 OR name = ?3",
+     rusqlite::params![ani.cur_ep, ani.id, ani.nome],
+    )?;
+    Ok(())
+}
+
+pub fn remove_ani(conn: &Connection, ani: &Anime) -> ResultSql<bool>{
+    let lines = conn.execute("DELETE FROM anime WHERE id = ?1 OR name = ?2",
+     rusqlite::params![ani.id,ani.nome],
+    )?;
+    if lines > 0{
+        Ok(true)
+    }
+    else {
+        Ok(false)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,117 @@
 mod db;
-use std::env;
+use std::{env, io, process};
 use rusqlite::Result as ResultSql;
 
-use db::{add_data, init_db, new_db, show_db, Anime};
+use db::{Anime};
+
+const HELP_TEXT: &str = "\
+Usage: cargo run -- <COMMAND> <ANIME_NAME> [ARGS]
+
+Commands:
+  add <name> <total_eps>   Adds a new anime to the database.
+  up <name>                Updates the watched episodes for a specific anime.
+  rm <name>                Removes an anime from the database.
+  show/-s                  Show the local database. 
+
+Examples:
+  anime-cli add \"One Piece\" 1070
+  *****     up \"Naruto\"
+  *****     up \"id_anime\"  
+  *****     rm \"Bleach\"
+  *****     rm \"id_anime\" 
+  *****     show/-s
+  ";
 
 fn main()-> ResultSql<()>{
-    let argv: Vec<String> = env::args().collect();
-    let connec = new_db()?;
-    init_db(&connec)?; 
+    let args: Vec<String> = env::args().collect();
+    let connec = db::new_db()?;
+    db::init_db(&connec)?; 
+    match args.len() {
+        4 => {
+            let opt: &str = &args[1];
+            let anime_name: &str = &args[2];
+            let num_ep: u32 = args[3].parse().unwrap_or(0);
+            match opt{
+                "add" =>{
+                    let mut ani = Anime::new(anime_name, num_ep);
+                    let insert_ani = db::add_data(&connec, &mut ani)?;
+                    if insert_ani{
+                        db::show_db(&connec)?;
+                    }else {
+                        println!("Anime already exists in your database: {}", ani.nome);
+                    }
+                },
+                _ => {
+                    eprintln!("Error: command unknow");
+                    eprintln!("Use 'anime help' to see the available commands");
+                    process::exit(1);
+                }
+            }
+        },
+        3 =>{
+            let opt: &str = &args[1];
+            let anime_name: &str = &args[2];
+            match db::search_ani(&connec, anime_name) {
+               Ok(mut anime) => match opt {
+                   "up" =>{
+                        println!("Enter the number of epsodes seen: ");
+                        let mut input = String::new();
+                        io::stdin()
+                        .read_line(&mut input)
+                        .expect("Error enter a valide number");
 
-    if let Some(arg1) = argv.get(1){
-       let mut ani = Anime::new(arg1, 12);
-       let insert_ani = add_data(&connec, &mut ani)?;
-       if insert_ani{
-       show_db(&connec)?; 
-       }
-       else {
-           println!("Anime already exists in your database: {}", ani.nome);
-       }
-    }
-    else{
-        println!("no args passed");
+                        let num_seen: u32 = input.trim().parse().expect("Erro ao converter");
+                        db::update_ep(&connec, &mut anime, num_seen)?;
+                        db::show_db(&connec)?; 
+                   },
+                   "rm" =>{
+                        let deleted_item = db::remove_ani(&connec, &anime)?;
+                        if deleted_item{
+                            println!("Anime '{}' removed successfully", anime.nome);
+                            db::show_db(&connec)?;
+                        }
+                        else{
+                            println!("Error: unable to remove the anime.");
+                        }
+                   },
+                   _ => {
+                    eprintln!("Error: command unknow");
+                    eprintln!("Use 'anime help' to see the available commands");
+                    process::exit(1);
+                }
+               }
+               Err(_) =>{
+                println!("Anime not found in database, make sure you entered the correct name or id");
+               }
+            }
+        }
+        2 => {
+            let opt: &str = &args[1];
+            match opt {
+                "show" => {
+                    db::show_db(&connec)?;
+                }
+                "-s" => {
+                    db::show_db(&connec)?;
+                }
+                "-h" =>{
+                    println!("{}", HELP_TEXT);
+                }
+                "help" =>{
+                    println!("{}", HELP_TEXT);
+                }
+                _ => {
+                    eprintln!("Error: command unknow");
+                    eprintln!("Use 'anime help' to see the available commands");
+                    process::exit(1);
+                }
+            }
+        }
+        _ => {
+                eprintln!("Error: command unknow");
+                eprintln!("Use 'anime help' to see the available commands");
+                process::exit(1);
+        }
     }
     Ok(())
 }
